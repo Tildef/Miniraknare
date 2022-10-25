cdcc5b486950216f264b17368f085691094520da
Tildef <93967968+Tildef@users.noreply.github.com>
2022-10-25 09:14:34 +0300

    Lagt till projektet till det här repositoryt

---
+++ P7_Miniraknare_3/P7_Miniraknare/Form1.Designer.vb
@@ -0,0 +1,289 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtInmatning = New System.Windows.Forms.TextBox()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnDecimal = New System.Windows.Forms.Button()
+        Me.btnSumma = New System.Windows.Forms.Button()
+        Me.btnAddition = New System.Windows.Forms.Button()
+        Me.btnSubtraktion = New System.Windows.Forms.Button()
+        Me.btnMultiplikation = New System.Windows.Forms.Button()
+        Me.btnRensaAllt = New System.Windows.Forms.Button()
+        Me.btnRensaInmatning = New System.Windows.Forms.Button()
+        Me.btnDivision = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtInmatning
+        '
+        Me.txtInmatning.BackColor = System.Drawing.Color.LightGray
+        Me.txtInmatning.Location = New System.Drawing.Point(36, 36)
+        Me.txtInmatning.MaxLength = 8
+        Me.txtInmatning.Multiline = True
+        Me.txtInmatning.Name = "txtInmatning"
+        Me.txtInmatning.Size = New System.Drawing.Size(234, 38)
+        Me.txtInmatning.TabIndex = 0
+        Me.txtInmatning.Text = "0"
+        Me.txtInmatning.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.Azure
+        Me.btn1.Location = New System.Drawing.Point(12, 232)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(45, 45)
+        Me.btn1.TabIndex = 1
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.Azure
+        Me.btn2.Location = New System.Drawing.Point(63, 232)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(45, 45)
+        Me.btn2.TabIndex = 2
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.Azure
+        Me.btn3.Location = New System.Drawing.Point(114, 232)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(45, 45)
+        Me.btn3.TabIndex = 3
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.Azure
+        Me.btn4.Location = New System.Drawing.Point(12, 171)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(45, 45)
+        Me.btn4.TabIndex = 4
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.Azure
+        Me.btn5.Location = New System.Drawing.Point(63, 171)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(45, 45)
+        Me.btn5.TabIndex = 5
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.Azure
+        Me.btn6.Location = New System.Drawing.Point(114, 171)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(45, 45)
+        Me.btn6.TabIndex = 6
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.Azure
+        Me.btn7.Location = New System.Drawing.Point(12, 110)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(45, 45)
+        Me.btn7.TabIndex = 7
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.Azure
+        Me.btn8.Location = New System.Drawing.Point(63, 110)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(45, 45)
+        Me.btn8.TabIndex = 8
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.Azure
+        Me.btn9.Location = New System.Drawing.Point(114, 110)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(45, 45)
+        Me.btn9.TabIndex = 9
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.Color.Azure
+        Me.btn0.Location = New System.Drawing.Point(12, 294)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(86, 45)
+        Me.btn0.TabIndex = 10
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'btnDecimal
+        '
+        Me.btnDecimal.BackColor = System.Drawing.Color.Azure
+        Me.btnDecimal.Location = New System.Drawing.Point(114, 294)
+        Me.btnDecimal.Name = "btnDecimal"
+        Me.btnDecimal.Size = New System.Drawing.Size(45, 45)
+        Me.btnDecimal.TabIndex = 11
+        Me.btnDecimal.Text = "."
+        Me.btnDecimal.UseVisualStyleBackColor = False
+        '
+        'btnSumma
+        '
+        Me.btnSumma.BackColor = System.Drawing.Color.PaleGoldenrod
+        Me.btnSumma.Location = New System.Drawing.Point(189, 294)
+        Me.btnSumma.Name = "btnSumma"
+        Me.btnSumma.Size = New System.Drawing.Size(45, 45)
+        Me.btnSumma.TabIndex = 12
+        Me.btnSumma.Text = "="
+        Me.btnSumma.UseVisualStyleBackColor = False
+        '
+        'btnAddition
+        '
+        Me.btnAddition.BackColor = System.Drawing.Color.DarkSalmon
+        Me.btnAddition.Location = New System.Drawing.Point(189, 232)
+        Me.btnAddition.Name = "btnAddition"
+        Me.btnAddition.Size = New System.Drawing.Size(45, 45)
+        Me.btnAddition.TabIndex = 13
+        Me.btnAddition.Text = "+"
+        Me.btnAddition.UseVisualStyleBackColor = False
+        '
+        'btnSubtraktion
+        '
+        Me.btnSubtraktion.BackColor = System.Drawing.Color.DarkSalmon
+        Me.btnSubtraktion.Location = New System.Drawing.Point(189, 171)
+        Me.btnSubtraktion.Name = "btnSubtraktion"
+        Me.btnSubtraktion.Size = New System.Drawing.Size(45, 45)
+        Me.btnSubtraktion.TabIndex = 14
+        Me.btnSubtraktion.Text = "-"
+        Me.btnSubtraktion.UseVisualStyleBackColor = False
+        '
+        'btnMultiplikation
+        '
+        Me.btnMultiplikation.BackColor = System.Drawing.Color.DarkSalmon
+        Me.btnMultiplikation.Location = New System.Drawing.Point(189, 110)
+        Me.btnMultiplikation.Name = "btnMultiplikation"
+        Me.btnMultiplikation.Size = New System.Drawing.Size(45, 45)
+        Me.btnMultiplikation.TabIndex = 15
+        Me.btnMultiplikation.Text = "x"
+        Me.btnMultiplikation.UseVisualStyleBackColor = False
+        '
+        'btnRensaAllt
+        '
+        Me.btnRensaAllt.BackColor = System.Drawing.Color.Orange
+        Me.btnRensaAllt.Location = New System.Drawing.Point(249, 294)
+        Me.btnRensaAllt.Name = "btnRensaAllt"
+        Me.btnRensaAllt.Size = New System.Drawing.Size(45, 45)
+        Me.btnRensaAllt.TabIndex = 16
+        Me.btnRensaAllt.Text = "AC"
+        Me.btnRensaAllt.UseVisualStyleBackColor = False
+        '
+        'btnRensaInmatning
+        '
+        Me.btnRensaInmatning.BackColor = System.Drawing.Color.Orange
+        Me.btnRensaInmatning.Location = New System.Drawing.Point(249, 232)
+        Me.btnRensaInmatning.Name = "btnRensaInmatning"
+        Me.btnRensaInmatning.Size = New System.Drawing.Size(45, 45)
+        Me.btnRensaInmatning.TabIndex = 17
+        Me.btnRensaInmatning.Text = "C"
+        Me.btnRensaInmatning.UseVisualStyleBackColor = False
+        '
+        'btnDivision
+        '
+        Me.btnDivision.BackColor = System.Drawing.Color.DarkSalmon
+        Me.btnDivision.Location = New System.Drawing.Point(249, 110)
+        Me.btnDivision.Name = "btnDivision"
+        Me.btnDivision.Size = New System.Drawing.Size(45, 45)
+        Me.btnDivision.TabIndex = 18
+        Me.btnDivision.Text = "/"
+        Me.btnDivision.UseVisualStyleBackColor = False
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Ivory
+        Me.ClientSize = New System.Drawing.Size(304, 352)
+        Me.Controls.Add(Me.btnDivision)
+        Me.Controls.Add(Me.btnRensaInmatning)
+        Me.Controls.Add(Me.btnRensaAllt)
+        Me.Controls.Add(Me.btnMultiplikation)
+        Me.Controls.Add(Me.btnSubtraktion)
+        Me.Controls.Add(Me.btnAddition)
+        Me.Controls.Add(Me.btnSumma)
+        Me.Controls.Add(Me.btnDecimal)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.txtInmatning)
+        Me.Name = "form1"
+        Me.Text = "Kalkylator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtInmatning As TextBox
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnDecimal As Button
+    Friend WithEvents btnSumma As Button
+    Friend WithEvents btnAddition As Button
+    Friend WithEvents btnSubtraktion As Button
+    Friend WithEvents btnMultiplikation As Button
+    Friend WithEvents btnRensaAllt As Button
+    Friend WithEvents btnRensaInmatning As Button
+    Friend WithEvents btnDivision As Button
+End Class
---
+++ P7_Miniraknare_3/P7_Miniraknare/Form1.vb
@@ -0,0 +1,232 @@
+﻿Imports System.Drawing.Text
+Imports System.Reflection.Emit
+Imports System.Runtime.InteropServices
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar
+
+Public Class form1
+    Dim Forsta As Double
+    Dim Andra As Decimal
+    Dim Operatorer As Integer
+    Dim Operator_valjare As Boolean = False
+
+    Private Sub form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Nollställer inmatningsrutan vid uppstart av appen
+        txtInmatning.Text = ""
+
+    End Sub
+
+    Private Sub txtInmatning_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtInmatning.KeyPress
+        'Tillåter punkt
+        If e.KeyChar = "."c AndAlso InStr(sender.text, ".") > 0 Then
+            e.Handled = True
+        End If
+
+        'Tillåter siffor
+        If (e.KeyChar < "0"c OrElse e.KeyChar > "9"c) AndAlso e.KeyChar <> "."c AndAlso e.KeyChar <> vbBack Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+    Private Sub txtInmatning_Click(sender As Object, e As EventArgs) Handles txtInmatning.Click, btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+        'Begränsar att man bara kan skriva in 8 tal
+        If txtInmatning.TextLength >= Int(8) Then
+            MsgBox("Max 8 tecken")
+        End If
+
+    End Sub
+
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 0 till
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "0"
+
+        End If
+
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 1 till annars är det 1
+
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "1"
+        Else
+            txtInmatning.Text = "1"
+        End If
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 2 till annars är det 2
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "2"
+        Else
+            txtInmatning.Text = "2"
+        End If
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 3 till annars är det 3
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "3"
+        Else
+            txtInmatning.Text = "3"
+        End If
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 4 till annars är det 4
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "4"
+        Else
+            txtInmatning.Text = "4"
+        End If
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 5 till annars är det 5
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "5"
+        Else
+            txtInmatning.Text = "5"
+        End If
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 6 till annars är det 6
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "6"
+        Else
+            txtInmatning.Text = "6"
+        End If
+
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 7 till annars är det 7
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "7"
+        Else
+            txtInmatning.Text = "7"
+        End If
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 8 till annars är det 8
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "8"
+        Else
+            txtInmatning.Text = "8"
+        End If
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        'Om inmatningsrutan inte är lika med 0 så läggs 9 till annars är det 9
+        If txtInmatning.Text <> "0" Then
+            txtInmatning.Text += "9"
+        Else
+            txtInmatning.Text = "9"
+        End If
+    End Sub
+
+    Private Sub btnDecimal_Click(sender As Object, e As EventArgs) Handles btnDecimal.Click
+        'Om inmatningsrutan är tom så när man trycker på decimaltecknet läggs 0 till före
+        If txtInmatning.Text = "" Then
+            txtInmatning.Text += "0."
+            'Annars läggs bara decimaltecknet till
+        ElseIf Not (txtInmatning.Text.Contains(".")) Then
+            txtInmatning.Text += "."
+
+        End If
+
+    End Sub
+
+    Private Sub btnSumma_Click(sender As Object, e As EventArgs) Handles btnSumma.Click
+        'Om man valt ett räknesätt så kan uträkningen ske
+        If Operator_valjare = True Then
+            Andra = txtInmatning.Text
+
+            'Operatorn för addition
+            If Operatorer = 1 Then
+                'Avrundar svaret så det inte är mer än 8 tecken 
+                txtInmatning.Text = Math.Round(Forsta + Andra, 7)
+
+                'Operatorn för subtraktion
+            ElseIf Operatorer = 2 Then
+                'Avrundar svaret så det inte är mer än 8 tecken 
+                txtInmatning.Text = Math.Round(Forsta - Andra, 7)
+
+                'Operatorn för multiplikation
+            ElseIf Operatorer = 3 Then
+                'Avrundar svaret så det inte är mer än 8 tecken 
+                txtInmatning.Text = Math.Round(Forsta * Andra, 7)
+
+            Else
+                'Om man försöker dela något med 0 så skrivs ett felmeddelande ut
+                If Andra = 0 Then
+                    txtInmatning.Text = "Error, kan inte dela med 0"
+                Else
+                    'Operatorn för divition
+                    txtInmatning.Text = Math.Round(Forsta / Andra, 7)     'Avrundar svaret så det inte är mer än 8 tecken 
+                End If
+            End If
+            Operator_valjare = False
+        End If
+
+
+    End Sub
+
+
+    Private Sub btnRensaAllt_Click(sender As Object, e As EventArgs) Handles btnRensaAllt.Click
+
+        'Rensar allt
+        txtInmatning.Clear()
+
+
+    End Sub
+
+    Private Sub btnRensaInmatning_Click(sender As Object, e As EventArgs) Handles btnRensaInmatning.Click
+        'Rensar inmatningsrutan
+        txtInmatning.Text = ""
+
+    End Sub
+
+    Private Sub btnAddition_Click(sender As Object, e As EventArgs) Handles btnAddition.Click
+        'Rätt operator väljs och texten blir till 0
+        Forsta = Convert.ToDouble(txtInmatning.Text)
+        txtInmatning.Text = "0"
+        Operator_valjare = True
+        Operatorer = 1
+
+
+
+    End Sub
+
+    Private Sub btnSubtraktion_Click(sender As Object, e As EventArgs) Handles btnSubtraktion.Click
+        'Rätt operator väljs och texten blir till 0
+        Forsta = txtInmatning.Text
+        txtInmatning.Text = "0"
+        Operator_valjare = True
+        Operatorer = 2
+
+    End Sub
+
+    Private Sub btnMultiplikation_Click(sender As Object, e As EventArgs) Handles btnMultiplikation.Click
+        'Rätt operator väljs och texten blir till 0
+        Forsta = txtInmatning.Text
+        txtInmatning.Text = "0"
+        Operator_valjare = True
+        Operatorer = 3
+    End Sub
+
+    Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+        'Rätt operator väljs och texten blir till 0
+        Forsta = txtInmatning.Text
+        txtInmatning.Text = "0"
+        Operator_valjare = True
+        Operatorer = 4
+
+    End Sub
+
+End Class
